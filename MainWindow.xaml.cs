cd764238c3d4d6d4f0eb8e9712f37a8918bf3ae4
nikita.prigolovkin <vitalii.prigolovkin@gmail.com>
2023-07-02 21:58:40 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Key_Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private RelayCommand _redCommand;
+        private RelayCommand _greenCommand;
+        private RelayCommand _blueCommand;
+        private string _buttonColor;
+
+        public ICommand RedCommand
+        {
+            get
+            {
+                if (_redCommand == null)
+                    _redCommand = new RelayCommand(param => ButtonColor = "Red");
+                return _redCommand;
+            }
+        }
+
+        public ICommand GreenCommand
+        {
+            get
+            {
+                if (_greenCommand == null)
+                    _greenCommand = new RelayCommand(param => ButtonColor = "Green");
+                return _greenCommand;
+            }
+        }
+
+        public ICommand BlueCommand
+        {
+            get
+            {
+                if (_blueCommand == null)
+                    _blueCommand = new RelayCommand(param => ButtonColor = "Blue");
+                return _blueCommand;
+            }
+        }
+
+        public string ButtonColor
+        {
+            get { return _buttonColor; }
+            set
+            {
+                _buttonColor = value;
+                OnPropertyChanged("ButtonColor");
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
